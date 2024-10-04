ba3c4fe15824ce8bff0ef42dec4dd96f1562f705
chandima2000 <hychandima2000@gmail.com>
2024-10-04 17:29:27 +0530

    update the controller for getAllProducts

--- src/main/java/org/chandima/backend/controller/ProductController.java
+++ src/main/java/org/chandima/backend/controller/ProductController.java
@@ -1,15 +1,24 @@
 package org.chandima.backend.controller;
 
+import org.chandima.backend.model.Product;
+import org.chandima.backend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
     @GetMapping("/products")
-    public String getProducts(){
-        return "All products";
+    public List<Product> getProducts(){
+        return productService.getAllProducts();
     }
 }
